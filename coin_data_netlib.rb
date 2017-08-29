a166516da87df90af1b949caa56e4a85833c05bf
Soonho Kong <soonho.kong@tri.global>
2017-08-28 22:11:01 -0400

    Update coin_data_netlib.rb

--- coin_data_netlib.rb
+++ coin_data_netlib.rb
@@ -3,6 +3,11 @@ class CoinDataNetlib < Formula
   homepage "http://www.coin-or.org/download/pkgsource/Data"
   url "http://www.coin-or.org/download/pkgsource/Data/Data-Netlib-1.2.6.tgz"
   sha256 "dd687e5a35087ae0da825c5e24832b2ebaacc36a2ee73df084d4ef92e575e1ab"
+  
+  bottle do
+    root_url 'https://dl.bintray.com/dreal/homebrew-coinor'
+    sha256 "abbf720fa8aeb4d3014157bd332390866c6adf544d3e8543a7dd5e1c49166a62" => :sierra
+  end
 
   def install
     system "./configure", "--disable-debug",
